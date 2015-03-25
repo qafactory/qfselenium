303352f31b2bf722ea78e29cf222dc9db92e95ef
qafactory <emma.shymilovuch@gmail.com>
2015-03-25 20:56:30 +0300

    First main.java added

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+
+        String[] s1 = {"a", "b", "c"};
+        String[] s2 = {"a", "d", "c"};
+
+        System.out.print(s1[0] + " - " + s2[0] + ", " + s1[1] + " - " + s2[1]+ ", " + s1[2] + " - " + s2[2]);
+
+
+
+
+
+
+
+    }
+}
