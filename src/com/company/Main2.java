990e8f3264c31da00f3a20d177caa8516063092f
qafactory <emma.shymilovuch@gmail.com>
2015-03-25 21:58:17 +0300

    First main.java added

---
+++ src/com/company/Main2.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by Admin on 25.03.15.
+ */
+public class Main2 {
+    public static void main(String[] args) {
+
+
+        if (args[0].equals("Hello") && args[1].equals("Class")){
+            System.out.println("Success");
+        } else {
+            System.out.println("Good bye");
+        }
+
+
+    }
+}
