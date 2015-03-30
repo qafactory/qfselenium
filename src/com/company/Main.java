8b4666539bd1064ebdb3b04cd376007d86d051c0
qafactory <emma.shymilovuch@gmail.com>
2015-03-30 21:10:46 +0400

    Open Chrome

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,15 @@ public class Main {
 
 
 
-        String[] s1 = {"a", "b", "c"};
-        String[] s2 = {"a", "d", "c"};
+       // String[] s1 = {"a", "b", "c"};
+       // String[] s2 = {"a", "d", "c"};
 
-        System.out.print(s1[0] + " - " + s2[0] + ", " + s1[1] + " - " + s2[1]+ ", " + s1[2] + " - " + s2[2]);
+       // System.out.print(s1[0] + " - " + s2[0] + ", " + s1[1] + " - " + s2[1]+ ", " + s1[2] + " - " + s2[2]);
+
+        String s = "Hello";
+        System.out.println(s.contains("l"));
+
+        String g = new String();
 
 
 
