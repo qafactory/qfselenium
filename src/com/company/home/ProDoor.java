eaad54880dfd0bcffeee179eae45843119ed9e5a
emma <emma.shymilovuch@gmail.com>
2015-04-01 16:55:31 +0300

    ProDoor class (homework)

--- src/com/company/home/ProDoor.java
+++ src/com/company/home/ProDoor.java
@@ -6,14 +6,55 @@ package com.company.home;
 public class ProDoor {
     int height;
     int width;
-    boolean isOpened;
-    boolean isLocked;
+    boolean opened;
+    boolean locked;
 
-    public void ProDoor(int height, int width, boolean isOpened, boolean isLocked){
-
+    public ProDoor(int height, int width, boolean opened, boolean locked) {
+        this.height = height;
+        this.width = width;
+        this.opened = opened;
+        this.locked = locked;
     }
 
+    @Override
+    public String toString(){
+        String str = "Size:" + height + "x" + width + " mm\n";
+        str += "Opened: " + (opened ? "Yes" : "No") + "\n";
+        str += "Locked: " + (locked ? "Yes" : "No") + "\n";
+        return str;
+    }
 
+    public void lock(){
+        locked = true;
+        //opened = false;
+    }
 
+    public boolean isLocked(){
+        return locked;
+    }
+
+    public void close(){
+        if (locked){
+            System.out.println("Lock is in the way. Cannot close!");
+        } else{
+            opened = false;
+        }
+    }
+
+    public void open(){
+        if (locked){
+            System.out.println("Door is locked. Cannot open!");
+        } else{
+            opened = true;
+        }
+    }
+
+    public boolean isClosed(){
+        return !opened;
+    }
+
+    public void unLock(){
+        locked = false;
+    }
 
 }
--- src/com/company/home/UseProDoor.java
+++ src/com/company/home/UseProDoor.java
@@ -4,4 +4,39 @@ package com.company.home;
  * Created by Emma on 4/1/2015.
  */
 public class UseProDoor {
+
+    public static void main(String[] args) {
+        ProDoor door = new ProDoor(2000, 900, true, false);
+        String doorInfo = door.toString();
+        System.out.println("Door info:\n" + doorInfo);
+
+        ProDoor door2 = new ProDoor(1900, 800, false, true);
+        System.out.println("Door info:\n" + door2.toString());
+
+        door.lock();
+        System.out.println(door.isLocked()); //true
+
+        door.close(); // Lock is in the way. Cannot close!
+
+        System.out.println(door.isClosed()); // false
+
+        System.out.println("Door info:\n" + door.toString());
+
+        door.unLock();
+        System.out.println(door.isLocked()); // false
+
+        door.close();
+        System.out.println(door.isClosed()); // true
+
+        door2.open(); // Door is locked. Cannot open!
+        door2.unLock();
+        door2.open();
+        System.out.println("Door info:\n" + door2.toString());
+
+
+
+
+    }
+
+
 }
