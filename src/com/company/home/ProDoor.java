81a632ee664d8d3fa47eebbedca726f7d166bb69
Prof <profautom@gmail.com>
2015-04-01 09:11:22 +0300

    Homework

---
+++ src/com/company/home/ProDoor.java
@@ -0,0 +1,19 @@
+package com.company.home;
+
+/**
+ * Created by Emma on 4/1/2015.
+ */
+public class ProDoor {
+    int height;
+    int width;
+    boolean isOpened;
+    boolean isLocked;
+
+    public void ProDoor(int height, int width, boolean isOpened, boolean isLocked){
+
+    }
+
+
+
+
+}
---
+++ src/com/company/home/TestClass.java
@@ -0,0 +1,18 @@
+package com.company.home;
+
+/**
+ * Created by Emma on 4/1/2015.
+ */
+public class TestClass {
+    String a = "Hello World";
+
+    public void say(){
+        System.out.println(a);
+    }
+
+    public void update(String u){
+        a = u;
+
+    }
+
+}
