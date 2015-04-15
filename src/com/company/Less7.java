5db8622c706fd52a769b02642cd7f118bc6e7464
emma <emma@g.com>
2015-04-15 16:07:27 +0300

    Homework

--- src/com/company/Less7.java
+++ src/com/company/Less7.java
@@ -19,13 +19,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 @RunWith(JUnit4.class)
 public class Less7 {
 
-    public WebDriver driver;
+    public static WebDriver driver;
 
-//    @Test
-//    public void test1(){
-//        Assert.fail();
-//    }
-//
 //    @Test
 //    public void test2(){
 //        int a =10;
@@ -99,10 +94,29 @@ public class Less7 {
     }
 
     @Test
-    public void title() {
+    public void titleP() {
         Assert.assertEquals("Password generator", driver.getTitle());
 
     }
+
+
+    //Homework
+    @Test
+    public void homework(){
+        master("12345678");
+        site("gmail.com");
+        generate();
+        System.out.println(password());
+        System.out.println(master());// 12345678
+        System.out.println(site()); // gmail.com
+        System.out.println(title()); // Password generator
+        System.out.println(button()); // Generate
+        site("");
+        generate();
+        System.out.println(password()); // 9Ixm2r5Xnm41Q@1a
+
+    }
+
 
     public static String gen(WebDriver driver, String p, String s){
         WebElement passw = driver.findElement(By.name("master"));
@@ -118,6 +132,50 @@ public class Less7 {
         return gPassw.getAttribute("value");
     }
 
+    public static void master(String p){
+        WebElement passw = driver.findElement(By.name("master"));
+        passw.clear();
+        passw.sendKeys(p);
+    }
+
+    public static void site(String s){
+        WebElement site = driver.findElement(By.name("site"));
+        site.clear();
+        site.sendKeys(s);
+
+    }
+
+    public static void generate(){
+        WebElement site = driver.findElement(By.name("site"));
+        site.sendKeys(Keys.ENTER);
+    }
+
+    public static String password(){
+        WebElement gPassw = driver.findElement(By.name("password"));
+        return gPassw.getAttribute("value");
+
+    }
+
+    public static String master(){
+        WebElement passw = driver.findElement(By.name("master"));
+        return passw.getAttribute("value");
+    }
+
+    public static String site(){
+        WebElement site = driver.findElement(By.name("site"));
+        return site.getAttribute("value");
+    }
+
+    public static String title(){
+        return driver.getTitle();
+    }
+
+    public static String button(){
+        WebElement btn = driver.findElements(By.tagName("input")).get(2);
+        return btn.getAttribute("value");
+    }
+
+
     @After //Run after every @Test
     public void clean(){
         driver.quit();
@@ -125,11 +183,11 @@ public class Less7 {
 
     @Before //Run before every @Test
     public void start(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Automation\\Soft\\chromedriver_win32\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "C:\\Automation\\Soft\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/home/emma/Documents/Tools/selenium/chromedriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://angel.net/~nic/passwd.current.html");
-
     }
 
 
